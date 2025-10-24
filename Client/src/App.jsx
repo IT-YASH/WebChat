d568e37553f44f89199d64f46e5cbad84029d89f
IT-YASH <pyash7646@gmail.com>
2025-10-24 22:10:45 +0530

    add vercel

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -10,7 +10,7 @@ const App = () => {
   const { authUser } = useContext(AuthContext);
 
   return (
-    <div className="bg-[url('./src/assets/bgImage.svg')] bg-contain">
+    <div className="bg-[url('/bgImage.svg')] bg-contain">
       <Toaster />
       <Routes>
         {/* ✅ If logged in → show HomePage, else → go to Login */}
